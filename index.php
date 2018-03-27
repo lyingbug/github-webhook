f49684eeab7794b8ae5949bc8d269efcedd333a0
wizardchen <wizardchen@tencent.com>
2018-03-27 19:59:42 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
+require_once( './vendor/autoload.php' );
 
-file_put_contents( '/var/1.log', var_export( $_POST, true ) . PHP_EOL, FILE_APPEND );
+$raw_data = file_get_contents( 'php://input', 'r' );
+file_put_contents( '/usr/local/code/msg/log/1.log', var_export( $_POST, true ) . PHP_EOL, FILE_APPEND );
+
+
 if ( ! \app\tool\Tool::checkSignature( $_POST ) ) {
-	echo 'signature error';
-}
+	echo 'signature error : ' . 'request sign : '
+	     . \app\tool\Tool::getRequestSign() . 'server sign : ' . \app\tool\Tool::getSignature( $_POST );
 
+	return;
+}
+$data = json_decode( $raw_data, true );
+
+if ( ! in_array( $data['repository']['full_name'], array_keys( \app\Config::$triggerRepository ) ) ) {
+	echo 'this repository need nothing todo';
+
+	return;
+}
+echo exec( \app\Config::$triggerRepository[ $data['repository']['full_name'] ] );
